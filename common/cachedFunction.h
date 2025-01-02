d62babb4301987188ee26554f2f05d1e40f74d15
Artur Bicalho Saturnino <artur.bicalho.s@gmail.com>
2025-01-02 12:01:43 -0500

    day7

---
+++ common/cachedFunction.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <functional>
+#include <unordered_map>
+#include <utility>
+
+template<typename... Args>
+struct std::hash<std::tuple<Args...>>
+{
+
+    template <std::size_t I>
+    std::size_t hashHelper(const std::tuple<Args...>& args) const noexcept
+    {
+        return std::hash<std::tuple_element_t<I, std::tuple<Args...>>>{}(std::get<I>(args)) ^ (hashHelper<I-1>(args) << 1);
+    }
+
+    template <>
+    std::size_t hashHelper<0>(const std::tuple<Args...>& args) const noexcept
+    {
+        return std::hash<std::tuple_element_t<0, std::tuple<Args...>>>{}(std::get<0>(args));
+    }
+
+    std::size_t operator()(const std::tuple<Args...>& args) const noexcept
+    {
+        return hashHelper<std::tuple_size_v<std::tuple<Args...>> -1> (args);
+    }
+};
+
+
+template<typename R, typename ...Args>
+class CashedFunction
+{
+public:
+
+    using key_type = std::tuple<std::decay_t<Args>...>;
+
+    CashedFunction(const std::function<R(Args...)>& f):
+        m_f{f}
+    {
+    }
+
+    R operator()(Args... args)
+    {
+        key_type key = std::make_tuple(args ...);
+        if (m_cache.contains(key))
+            return m_cache[key];
+        return m_cache[key] = m_f(std::forward<decltype(args)>(args)...);
+    }
+
+
+
+private:
+
+    std::unordered_map<key_type, R> m_cache;
+    std::function<R(Args...)> m_f;
+
+};
---
+++ day7/day7.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <common/common.h>
+#include <common/cachedFunction.h>
+
+
+int64_t isValid(int64_t targetVal, const std::ranges::range auto& nums, bool allowConcat = false)
+{
+
+    std::function<bool(int64_t, int64_t)> contConfig
+        = [&contConfig, &nums, &allowConcat](int64_t target, int64_t pos)->int64_t
+        {
+            if (pos == 1)
+                return target == nums[pos - 1];
+
+            bool out = false;
+            if (target % nums[pos - 1] == 0 && target / nums[pos - 1] > 0)
+                out = out || contConfig(target / nums[pos - 1], pos - 1);
+            
+            if (out)
+                return true;
+            
+            if (target - nums[pos - 1] > 0)
+                out = out || contConfig(target - nums[pos - 1], pos - 1);
+            
+            if (out)
+                return true;
+
+            if (allowConcat)
+            {
+                int64_t temp = nums[pos - 1];
+                int64_t modTarget = target;
+                while (temp > 0)
+                {
+                    if (temp % 10 != modTarget % 10)
+                        return out;
+                    temp /= 10;
+                    modTarget /= 10;
+                }
+
+                out = out || contConfig(modTarget, pos - 1);
+
+            }
+
+            return out;
+        };
+
+    contConfig = CashedFunction<int64_t, int64_t, int64_t>(contConfig);
+    
+    return contConfig(targetVal, nums.size());
+
+}
+
+
+
+
+int64_t prob1(std::string inputFile)
+{
+    std::ifstream file(inputFile);
+    auto content = readFile(file);
+
+    int64_t tot = 0;
+
+    for (const auto& v : std::views::transform(content, readNumsl))
+    {
+        tot += isValid(v[0], std::views::drop(v, 1)) ? v[0] : 0;
+    }
+
+
+    return tot;
+}
+
+
+int64_t prob2(std::string inputFile)
+{
+    std::ifstream file(inputFile);
+    auto content = readFile(file);
+
+    int64_t tot = 0;
+
+    for (const auto& v : std::views::transform(content, readNumsl))
+    {
+        tot += isValid(v[0], std::views::drop(v, 1), true) ? v[0] : 0;
+    }
+
+
+    return tot;
+}
+
+int main()
+{
+    std::string test = "test.txt";
+    std::string input = "input.txt";
+
+    std::cout << "Test P1:" << std::endl << prob1(test) << std::endl;
+    std::cout << "Input P1:" << std::endl << prob1(input) << std::endl << std::endl;
+
+    std::cout << "Test P2:" << std::endl << prob2(test) << std::endl;
+    std::cout << "Input P2:" << std::endl << prob2(input) << std::endl;
+}
