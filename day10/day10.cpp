16454d2b3ed1d8309c041cb8adea536af3f6fb95
Artur Bicalho Saturnino <artur.bicalho.s@gmail.com>
2025-01-03 14:06:22 -0500

    day10 p2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -63,7 +63,49 @@ int64_t prob2(std::string inputFile)
 {
     std::ifstream file(inputFile);
     auto content = readFile(file);
-    return 0;
+
+    Grid<int> map(content | std::views::transform([](const std::string_view& str) {
+        return str | std::views::transform([](char c) {return static_cast<int>(c) - static_cast<int>('0'); });
+        }));
+
+
+    std::function<int64_t(int64_t, int64_t)> rating =
+        [&rating, &map](int64_t row, int64_t col)->int64_t
+        {
+            auto currElevation = map.get(row, col);
+            if (currElevation == 9)
+            {
+                return 1;
+            }
+
+            int64_t sum = 0;
+
+            for (const auto& d : cardinal_dirs)
+            {
+                int64_t mRow = row;
+                int64_t mCol = col;
+                moveInDir(d, mRow, mCol);
+                if (map.inBounds(mRow, mCol) && map.get(mRow, mCol) == currElevation + 1)
+                    sum += rating(mRow, mCol);
+            }
+            return sum;
+        };
+
+    rating = CachedFunction<int64_t, int64_t, int64_t>(rating);
+
+    int64_t tot = 0;
+
+    for (int64_t i = 0; i < map.nRows(); ++i)
+    {
+        for (int64_t j = 0; j < map.nCols(); ++j)
+        {
+            if (map.get(i, j) == 0)
+                tot += rating(i, j);
+
+        }
+    }
+
+    return tot;
 }
 
 int main()
