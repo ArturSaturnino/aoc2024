023b315747946542e02954448b6d110c41fb784b
Artur Bicalho Saturnino <artur.bicalho.s@gmail.com>
2025-05-16 08:29:53 +0200

    day 21 p2

--- day21/day21.cpp
+++ day21/day21.cpp
@@ -49,11 +49,20 @@ public:
         return m_keypad.get(pos);
     }
 
+    std::vector<char> getKeys() const
+    {
+        std::vector<char> keys;
+        for (char k : m_keypad)
+        {
+            if (k)
+                keys.push_back(k);
+        }
+
+        return keys;
+    }
+
 
 };
-
-
-
 
 KeyPad getNumericalKeypad()
 {
@@ -82,105 +91,71 @@ KeyPad getDirectionalKeypad()
      return keyPad;
  }
 
- std::vector<std::string> getNeighbors(std::string_view state, const KeyPad& directionalKeypad, const KeyPad& numericalKeypad)
+ auto getDistances(int nRobots, const KeyPad& directionalKeypad, const KeyPad& numericalKeypad)
  {
-    
-     std::vector<std::string> moves;
-     
-     int i = 0;
-     while ( i < state.size() && state[i] == 'A')
-         ++i;
-    
-     if (i + 1 < state.size())
-     {
-         const KeyPad& activeKeypad = i + 2 == state.size() ? numericalKeypad : directionalKeypad;
-         char activeState = state[i + 1];
-         auto activePos = activeKeypad.getPos(activeState);
-         auto proposedPos = moveInDir(getDir(state[i]), activePos);
-         if (activeKeypad.isValid(proposedPos))
-         {
-             std::string newState(state);
-             newState[i + 1] = activeKeypad.getState(proposedPos);
-             moves.push_back(newState);
-         } 
-     }
-      
-     const KeyPad& firstPad = state.size() == 0 ? numericalKeypad : directionalKeypad;
-     auto currPos = firstPad.getPos(state[0]);
-     for (const auto& d : firstPad.getValidMoves(currPos))
-     {
-         auto testMove = moveInDir(d, currPos);
-         char testState = firstPad.getState(testMove);
-         moves.emplace_back(state);
-         moves.back()[0] = testState;
-     }
-     return moves;
- }
-
- int64_t getPathLength(char startState, char endState, int nRobots, const KeyPad& directionalKeypad, const KeyPad& numericalKeypad, std::vector<std::string>& path)
- {
-     std::string startStateFull(nRobots, 'A');
-     std::string endStateFull(nRobots, 'A');
-
-     startStateFull.back() = startState;
-     endStateFull.back() = endState;
-
-     std::priority_queue<std::tuple<int64_t, std::string, std::string>> queue{};
-     std::set<std::string> visited;
-     std::map<std::string, std::string> parents;
-
-     queue.emplace(0, startStateFull, "");
-
-     while (!queue.empty())
-     {
-         auto [dist, state, parent] = queue.top();
-         dist *= -1;
-         queue.pop();
-
-         if (visited.contains(state))
-             continue;
-
-         visited.insert(state);
-         parents[state] = parent;
-
-         if (state == endStateFull)
-         {
-             std::string pathState = state;
-             while (!pathState.empty())
-             {
-                 path.push_back(pathState);
-                 pathState = parents[pathState];
-             }
-             path = std::vector(path.rbegin(), path.rend());
-
-             return dist + 1;
-         }
-
-         for (const auto& s : getNeighbors(state, directionalKeypad, numericalKeypad))
-         {
-             queue.push({ -dist -1, s, state});
-         }
-     }
-
-     throw std::runtime_error("No path found");
-
- }
-
-
- int64_t getShortestSeq(std::string_view digits, const KeyPad& directionalKeypad, const KeyPad& numericalKeypad, int nRobots)
- {
-     char lastState = 'A';
-     int64_t tot = 0;
-     std::string moves;
    
-     for (const char nextState : digits)
+     std::map<std::tuple<int, char, char>, int64_t> distances;
+     for (int i = 0; i < nRobots; ++i)
      {
-         std::vector<std::string> path;
-         tot += getPathLength(lastState, nextState, nRobots, directionalKeypad, numericalKeypad, path);
-         lastState = nextState;
+         const KeyPad& activeKeypad = i + 1 == nRobots ? numericalKeypad : directionalKeypad;
+         auto keys = activeKeypad.getKeys();
+
+         for (const auto& start : keys)
+         {
+             std::priority_queue<std::tuple<int64_t, std::string>> queue{};
+             std::set<std::string> visited;
+
+             queue.emplace(0, "A" + std::string(1,start));
+
+             while (!queue.empty())
+             {
+                 auto [ dist, state] = queue.top();
+                 dist *= -1;
+                 queue.pop();
+
+                 if (visited.contains(state))
+                     continue;
+
+                 if (state[0] == 'A')
+                     distances[{i, start, state[1]}] = dist;
+
+                 visited.insert(state);
+
+                                
+                 for (const auto& k : directionalKeypad.getKeys())
+                 {
+                     int64_t moveCost = distances[{i - 1, state[0], k}];
+                     std::string testStateFull = state;
+                     testStateFull[0] = k;
+                     if (!visited.contains(testStateFull))
+                        queue.push({ -dist - moveCost, testStateFull });
+                 }
+
+                 if (state[0] == 'A')
+                 {
+                     distances[{i, start, state[1]}] = dist;
+                 }
+                 else
+                 {
+                     auto currPosActive = activeKeypad.getPos(state[1]);
+                     auto posActiveProposed = moveInDir(getDir(state[0]), currPosActive);
+                     if (activeKeypad.isValid(posActiveProposed))
+                     {
+                         std::string testStateFull = state;
+                         testStateFull[1] = activeKeypad.getState(posActiveProposed);
+                         if (!visited.contains(testStateFull))
+                            queue.push({ -dist - 1, testStateFull });
+                     }
+                 }
+
+             }
+         }
+
      }
-     return tot;
+     return distances;
+
  }
+
 
 int64_t prob1(std::string inputFile)
 {
@@ -190,11 +165,20 @@ int64_t prob1(std::string inputFile)
     KeyPad numericalKeypad = getNumericalKeypad();
     KeyPad directionalKeypad = getDirectionalKeypad();
 
+    int nRobots = 3;
+    auto distMap = getDistances(nRobots, directionalKeypad, numericalKeypad);
     int64_t tot = 0;
+
     for (const auto& moves : content)
     {
-
-        tot += getShortestSeq(moves, directionalKeypad, numericalKeypad, 3) * std::stoll(moves.substr(0, moves.size() -1));
+        char lastState = 'A';
+        int64_t dist = 0;
+        for (const char nextState : moves)
+        {
+            dist += distMap[{nRobots - 1, lastState, nextState}] + 1;
+            lastState = nextState;
+        }
+        tot += dist * std::stoll(moves.substr(0, moves.size() -1));
     }
     return tot;
 }
@@ -208,10 +192,20 @@ int64_t prob2(std::string inputFile)
     KeyPad numericalKeypad = getNumericalKeypad();
     KeyPad directionalKeypad = getDirectionalKeypad();
 
+    int nRobots = 26;
+    auto distMap = getDistances(nRobots, directionalKeypad, numericalKeypad);
     int64_t tot = 0;
+
     for (const auto& moves : content)
     {
-        tot += getShortestSeq(moves, directionalKeypad, numericalKeypad, 10) * std::stoll(moves.substr(0, moves.size() - 1));
+        char lastState = 'A';
+        int64_t dist = 0;
+        for (const char nextState : moves)
+        {
+            dist += distMap[{nRobots - 1, lastState, nextState}] + 1;
+            lastState = nextState;
+        }
+        tot += dist * std::stoll(moves.substr(0, moves.size() - 1));
     }
     return tot;
 }
